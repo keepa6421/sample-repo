0c775cde9d6212d3a7b3911e779a3f0ac0493f5d
keepa6421 <152864098+keepa6421@users.noreply.github.com>
2023-12-23 09:23:05 +0545

    Added to main

---
+++ madlibs.py
@@ -0,0 +1,5 @@
+name=input("Enter your name:")
+age=input("Enter your age:")
+hobby=input("Enter your hobby:")
+madlibs=f"Hi my name is {name}. I am {age} years old and I love to {hobby}."
+print(madlibs)
